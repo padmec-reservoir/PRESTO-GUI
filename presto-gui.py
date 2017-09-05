bbdc5effc5bf5b2360dee44d908ad9da33aca831
João Antônio Cunha Silva <jacs@cin.ufpe.br>
2017-09-04 22:31:53 -0300

    New input interface and input system

--- presto-gui.py
+++ presto-gui.py
@@ -4,11 +4,14 @@ from PyQt5 import QtWidgets, QtGui
 from configobj import ConfigObj
 
 # Create list of parameters
-parameter_list = ["param1", "param2", "result"]
-dropdown_list = ["param1", "param2", "result", "dim"]
-unit_dict = {"dims" : ["Area", "Density", "Length", "Mass Flow", "Pressure",
-                       "Volume", "Volume Flow", "Weigth", "Weigth per Length"],
-             "Area" : ["acre", "square_foot", "square_inch", "meter ** 2"],
+parameter_list = ["Pressao inicial", "Pressao barometrica", "Poco produtor",
+                  "Poco injetor 1", "Poco injetor 2", "Tempo de injecao",
+                  "Densidade", "Viscosidade", "Altura", "Area", "Porosidade",
+                  "Permeabilidade"]
+dimension_list = ["Pressure", "Pressure", "Volume Flow", "Volume Flow",
+    	          "Volume Flow", "Time", "Density", "Viscosity", "Length",
+                  "Area", "Dimensionless", "Dimensionless"]
+unit_dict = {"Area" : ["acre", "square_foot", "square_inch", "meter ** 2"],
              "Density" : ["kilogram / meter ** 3", "pound / foot ** 3",
                           "pound / dry_gallon", "pound / inch ** 3"],
              "Length" : ["foot", "meter", "inch", "yard"],
@@ -21,19 +24,18 @@ unit_dict = {"dims" : ["Area", "Density", "Length", "Mass Flow", "Pressure",
              "Weigth" : ["kilogram", "pound"],
              "Weigth per Length" : ["kilogram / meter", "pound / foot",
                                     "pound / inch"],
-             "idle" : None}
+             "Time" : ["second", "minute", "hour", "day", "week", "month",
+                       "year"],
+             "Viscosity" : ["centipoise", "stokes", "rhe"],
+             "Dimensionless" : [""]}
 
 
 class Window(QtWidgets.QMainWindow):
-    def __init__(self, parameters=None, dropdown=None):
+    def __init__(self):
         super(Window, self).__init__()
-        if not parameters:
-            parameters = {}
-        self.param = parameters
-        if not dropdown:
-            dropdown = {}
-        self.dpdown = dropdown
-        self.setGeometry(50, 50, 300, 300)
+        self.value = {}
+        self.unit = {}
+        self.setGeometry(50, 50, 500, 500)
         self.setWindowTitle("PRESTO - Python Reservoir Simulation Toolbox")
         self.setWindowIcon(QtGui.QIcon('presto-logo.png'))
         self.ureg = pint.UnitRegistry()
@@ -80,20 +82,18 @@ class Window(QtWidgets.QMainWindow):
         name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
         file = ConfigObj(name[0])
         for x in parameter_list:
-            self.param[x] = float(file["parameters"][x])
-        for x in dropdown_list:
-            self.dpdown[x] = file["dropdowns"][x]
+            self.value[x] = file["values"][x]
+            self.unit[x] = file["units"][x]
         self.table_widget.update_parameters()
 
     def save_file(self):
         name = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File')
         file = ConfigObj(name[0])
-        file["parameters"] = {}
+        file["values"] = {}
+        file["units"] = {}
         for x in parameter_list:
-            file["parameters"][x] = self.param[x]
-        file["dropdowns"] = {}
-        for x in dropdown_list:
-            file["dropdowns"][x] = self.dpdown[x]
+            file["values"][x] = self.value[x]
+            file["units"][x] = self.unit[x]
         file.write()
 
 
@@ -110,125 +110,91 @@ class MyTableWidget(QtWidgets.QTabWidget):
         super(MyTableWidget, self).__init__(parent)
         self.parent = parent
         # Define layout
-        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout = QtWidgets.QGridLayout(self)
         # Make first tab
         self.tab1 = QtWidgets.QWidget()
         # Define layout of tab 1
-        self.tab1.layout = QtWidgets.QFormLayout(self.tab1)
-        # First Row
-        self.tab1.row1_label = QtWidgets.QLabel("Dimension", self.tab1)
-        self.tab1.row1_dim = MyComboBox(self.tab1, parent.ureg, "dims")
-        self.tab1.row1_dim.currentTextChanged.connect(self.get_dim)
-        self.tab1.layout.addRow(self.tab1.row1_label, self.tab1.row1_dim)
-        # Second Row
-        self.tab1.row2 = QtWidgets.QWidget()
-        self.tab1.row2.label = QtWidgets.QLabel("Parameters", self.tab1.row2)
-        self.tab1.row2.layout = QtWidgets.QGridLayout(self.tab1.row2)
-        # Creating and binding functions to text input and dropdown 1
-        self.tab1.row2.prm1 = QtWidgets.QLineEdit(self.tab1.row2)
-        self.tab1.row2.prm1.textEdited.connect(self.get_param1)
-        self.tab1.row2.prm1_dpdown = MyComboBox(self.tab1.row2, parent.ureg, "idle")
-        self.tab1.row2.prm1_dpdown.currentTextChanged.connect(self.get_dpdown1)
-        # Creating and binding functions to text input and dropdown 2
-        self.tab1.row2.prm2 = QtWidgets.QLineEdit(self.tab1.row2)
-        self.tab1.row2.prm2.textEdited.connect(self.get_param2)
-        self.tab1.row2.prm2_dpdown = MyComboBox(self.tab1.row2, parent.ureg, "idle")
-        self.tab1.row2.prm2_dpdown.currentTextChanged.connect(self.get_dpdown2)
-        # Setting up the layout of second row
-        self.tab1.row2.layout.addWidget(self.tab1.row2.prm1, 1, 1)
-        self.tab1.row2.layout.addWidget(self.tab1.row2.prm1_dpdown, 1, 2)
-        self.tab1.row2.layout.addWidget(self.tab1.row2.prm2, 2, 1)
-        self.tab1.row2.layout.addWidget(self.tab1.row2.prm2_dpdown, 2, 2)
-        self.tab1.layout.addRow(self.tab1.row2.label, self.tab1.row2)
-        # Third row
-        self.tab1.row3Label = QtWidgets.QLabel("Result", self.tab1)
-        self.tab1.row3Result = QtWidgets.QLineEdit(self.tab1)
-        self.tab1.row3Result.setReadOnly(True)
-        self.tab1.layout.addRow(self.tab1.row3Label, self.tab1.row3Result)
+        self.tab1.layout = self.make_tab1_layout()
         # Add tabs to tab screen
         self.addTab(self.tab1, "Tab 1")
 
-    def get_param1(self, text):
-        try:
-            value = float(text)
-        except ValueError:
-            value = 0.0
-        self.parent.param["param1"] = value
-        self.set_result()
+    def make_labels(self, parent):
+        labels = {}
+        for x in parameter_list:
+            labels[x] = QtWidgets.QLabel(x, parent)
+        return labels
 
-    def get_param2(self, text):
-        try:
-            value = float(text)
-        except ValueError:
-            value = 0.0
-        self.parent.param["param2"] = value
-        self.set_result()
+    def make_inputs(self, parent):
+        inputs = {}
+        for x in parameter_list:
+            inputs[x] = QtWidgets.QLineEdit(parent)
+            inputs[x].textEdited.connect(self.get_value)
+        return inputs
 
-    def get_dpdown1(self, text):
-        if text == "-- Choose Unit":
-            text = ""
-        self.parent.dpdown["param1"] = text
-        self.set_result()
+    def make_dropdowns(self, parent):
+        boxes = {}
+        i = 0
+        for x in parameter_list:
+            boxes[x] = MyComboBox(parent, self.parent.ureg, dimension_list[i])
+            boxes[x].editTextChanged.connect(self.get_unit)
+            i = i + 1
+        return boxes
 
-    def get_dpdown2(self, text):
-        if text == "-- Choose Unit":
-            text = ""
-        self.parent.dpdown["param2"] = text
-        self.set_result()
+    def make_tab1_layout(self):
+        layout = QtWidgets.QGridLayout(self.tab1)
+        layout.labels = self.make_labels(self.tab1)
+        layout.inputs = self.make_inputs(self.tab1)
+        layout.boxes = self.make_dropdowns(self.tab1)
+        i = 0
+        j = 1
+        for x in parameter_list:
+            layout.addWidget(layout.labels[x], (i%6) + 1, j)
+            layout.addWidget(layout.inputs[x], (i%6) + 1, j + 1)
+            layout.addWidget(layout.boxes[x], (i%6) + 1, j + 2)
+            i = i + 1
+            if i >= 6:
+                j = 4
+        return layout
 
-    def get_dim(self, text):
-        self.parent.dpdown["dim"] = text
-        while self.tab1.row2.prm1_dpdown.count() > 1:
-            self.tab1.row2.prm1_dpdown.removeItem(1)
-            self.tab1.row2.prm2_dpdown.removeItem(1)
-        if text != "-- Choose Dimension":
-            for x in unit_dict[text]:
-                self.tab1.row2.prm1_dpdown.addItem(x)
-                self.tab1.row2.prm2_dpdown.addItem(x)
+    def get_value(self, text):
+        for x in parameter_list:
+            try:
+                cur = float(self.tab1.layout.inputs[x].text())
+            except ValueError:
+                cur = 0.0
+            self.parent.value[x] = cur
+        self.dbg(1)
 
-    def set_result(self):
-        param1_value = self.parent.param["param1"]
-        param1_unit = self.parent.dpdown["param1"]
-        param2_value = self.parent.param["param2"]
-        param2_unit = self.parent.dpdown["param2"]
-        param1 = param1_value * self.parent.ureg(param1_unit)
-        param2 = param2_value * self.parent.ureg(param2_unit)
-        res = param1 * param2
-        self.parent.param["result"] = res.magnitude
-        self.parent.dpdown["result"] = str(res.units)
-        self.tab1.row3Result.setText(str(res.magnitude)+" "+str(res.units))
+    def get_unit(self, text):
+        for x in parameter_list:
+            self.parent.unit[x] = self.tab1.layout.boxes[x].currentText()
+        self.dbg(2)
 
     def update_parameters(self):
-        # Refactor this method
-        self.tab1.row2.prm1.setText(str(self.parent.param["param1"]))
-        self.tab1.row2.prm2.setText(str(self.parent.param["param2"]))
-        self.tab1.row3Result.setText(str(self.parent.param["result"]) + " " +
-                                     self.parent.dpdown["result"])
-        self.tab1.row1_dim.setCurrentText(self.parent.dpdown["dim"])
-        self.tab1.row2.prm1_dpdown.setCurrentText(self.parent.dpdown["param1"])
-        self.tab1.row2.prm2_dpdown.setCurrentText(self.parent.dpdown["param2"])
+        for x in parameter_list:
+            self.tab1.layout.inputs[x].setText(self.parent.value[x])
+            self.tab1.layout.boxes[x].setCurrentText(self.parent.unit[x])
+
+    def dbg(self, opt):
+        if opt == 1:
+            for x in parameter_list:
+                print(self.parent.value[x])
+        else:
+            for x in parameter_list:
+                print(self.parent.unit[x])
+        print()
 
 
 class MyComboBox(QtWidgets.QComboBox):
     def __init__(self, parent, ureg, dimension):
         super(MyComboBox, self).__init__(parent)
-        if dimension == "dims":
-            self.addItem("-- Choose Dimension")
-            for x in unit_dict[dimension]:
-                self.addItem(x)
-        else:
-            self.addItem("-- Choose Unit")
+        self.addItem("-- Choose Unit")
+        for x in unit_dict[dimension]:
+            self.addItem(x)
 
 
 if __name__ == '__main__':
-    # Create list of parameters values based om parameter list
-    parameters_values = {}
-    dropdown_values = {}
-    for x in parameter_list:
-        parameters_values[x] = 0.0
-    for x in dropdown_list:
-        dropdown_values[x] = ""
     app = QtWidgets.QApplication(sys.argv)
-    GUI = Window(parameters_values, dropdown_values)
+    GUI = Window()
     app.exec_()
     sys.exit()
