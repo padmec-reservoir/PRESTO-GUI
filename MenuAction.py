6f8f3085d47bcc15c4bb4f3d743a919130010990
Filipe-Cumaru <facsa@cin.ufpe.br>
2019-03-20 17:18:03 -0300

    Deleted old files

--- MenuAction.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QAction
-
-
-class MenuAction(QAction):
-    def __init__(self, parent, name, shortcut, status, function):
-        super(MenuAction, self).__init__(name, parent)
-        self.setShortcut(shortcut)
-        self.setStatusTip(status)
-        self.triggered.connect(function)
